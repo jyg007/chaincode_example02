606025926a41f23df42d5766cc84e121eac49fa4
Jean-Yves <girardjy@linux.local>
2017-01-11 22:27:49 +0100

    m

--- chaincode_example02.go
+++ chaincode_example02.go
@@ -104,12 +104,12 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	Bvalbytes, err := stub.GetState(B)
 	if err != nil {
 	
-		Bvalbytes=[]byte("0")
+		Bvalbytes=[]byte("100")
 		
 	//	return nil, errors.New("Failed to get state")
 	}
 	if Bvalbytes == nil {
-		return nil, errors.New("Entity not found")
+		return nil, errors.New("Entity not foud")
 	}
 	        fmt.Printf("Step3")
 	Bval, _ = strconv.Atoi(string(Bvalbytes))
